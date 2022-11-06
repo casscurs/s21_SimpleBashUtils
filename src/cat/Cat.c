011be8fb867c36e4e1ba82ea31e0c88e7c0a1075
Kolodyazhnyy D.A <danila.kolodyazhnyy@mail.ru>
2022-11-07 00:05:22 +0300

    getc

--- src/cat/Cat.c
+++ src/cat/Cat.c
@@ -1,3 +1,4 @@
+
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,32 +16,31 @@ typedef struct options {
 
 int main(int argc, char **argv) {
   opt options = {0, 0, 0, 0, 0, 0};
-  int length;
-  int flag = 0,flagin=0;
+  int flag = 0;
   int opt;
   FILE *fp = NULL;
-  int b_read;
-  size_t size = 10;
-  char *str;
   int b = 0;
   int opchar = 0;
   int opindex = 0;
+
   struct option opts[] = {
       {"squeeze-blank", no_argument, 0, 's'},
       {"number-nonblank", no_argument, 0, 'b'},
       {"E", no_argument, 0, 'e'},
       {"number", no_argument, 0, 'n'},
       {"T", no_argument, 0, 't'},
+      {"v", no_argument, 0, 'v'},
       {0, 0, 0, 0},
   };
 
-  while (-1 != (opchar = getopt_long(argc, argv, "+sbenstv", opts, &opindex))) {
+  while (-1 != (opchar = getopt_long(argc, argv, "+sbentv", opts, &opindex))) {
     switch (opchar) {
     case 'b':
       options.bflag = 1;
       break;
     case 'e':
       options.eflag = 1;
+      options.vflag = 1;
       break;
     case 'n':
       options.nflag = 1;
@@ -50,85 +50,86 @@ int main(int argc, char **argv) {
       break;
     case 't':
       options.tflag = 1;
+      options.vflag = 1;
+      break;
+    case 'E':
+      options.eflag = 1;
+      break;
+    case 'T':
+      options.tflag = 1;
       break;
     case 'v':
       options.vflag = 1;
       break;
     default:
-      printf("flag: %c\n", opchar);
+      printf("usage: cat [-benstuv] [file ...]\n");
     }
   }
   int currfile = optind;
-  str = (char *)malloc(size);
-
-  if(argc<=1){
-    flagin=1;
-  }
+  int currempt = 0;
+  int lastempt = 0;
+  int empt = 0;
+  int cur;
+  int Deleteflag = 0;
+  int prev = '\n';
+  int i = 1;
 
   while (currfile <= argc) {
     if (currfile != argc) {
-        fp = fopen(argv[currfile], "rb");
+      fp = fopen(argv[currfile], "r");
       if (fp == NULL) {
-        printf("%s: No such file or directory", argv[currfile]);
+        printf("cat: %s: No such file or directory\n", argv[currfile]);
         flag = 1;
-        currfile++;
-        continue;
+        break;
+        // currfile++;
+        // continue;
       }
     }
-    int lastempt = 0;
-    int i = 1;
-    int m=1;
-    while ((getline(&str, &size, (flagin ? stdin : fp))) != -1) {
-      length = strlen(str);
-      str[length - 1] = '\0';
-            if (options.eflag) {
-        length = strlen(str);
-        str[length] = '$';
-        str[length + 1] = '\0';
+    while ((cur = fgetc(fp)) != EOF) {
+      if (options.sflag && (cur == '\n') && (prev == '\n')) {
+        empt++;
+        if (empt > 1) {
+          Deleteflag = 1;
+        }
+      } else {
+        Deleteflag = 0;
+        empt = 0;
       }
-      if (options.sflag) {
-        length = strlen(str);
-        int currempt = 0;
-        if (length <= 1) {
-          currempt = 1;
-        }
-        if (lastempt && currempt) {
-          continue;
-        }
-        lastempt = currempt;
-      } 
-      if (options.bflag) {
-        if(strlen(str)>1){
-          printf("%*d\t", 6, m);
-          m++;
-        }
-      } else if (options.nflag) {
-        printf("%*d\t", 6, i);
-      }
-      if (options.tflag) {
-        length = strlen(str);
-        for (int k = 0; k <= length; k++) {
-          if (str[k] != '\0') {
-            if (str[k] == '\t') {
-              printf("^I");
-            } else
-              putchar(str[k]);
-          } else {
-            putchar('\n');
-          }
-        }
-      } else
-      // if (options.vflag){
-      //   length = strlen(str);
-      //   for(int k=0;k<=length;k++){
-      // }
-        fprintf(stdout, "%s\n", str);
 
-      i++;
+      if (!Deleteflag) {
+        if (((options.nflag && !options.bflag) ||
+             (options.bflag && cur != '\n')) &&
+            (prev == '\n')) {
+          printf("%*d\t", 6, i);
+          i++;
+        }
+      
+      if (options.tflag && cur == '\t') {
+        printf("^");
+        cur = 'I';
+      }
+
+      if (options.eflag && cur == '\n') {
+        printf("$");
+      }
+      if (options.vflag) {
+        while (cur >= 128) {
+          cur = cur - 128;
+        }
+        if (((cur < 9) && (cur >= 0)) || ((cur > 10) && (cur < 32))) {
+          printf("^");
+          cur = cur + 64;
+        } else if (cur == 127) {
+          printf("^");
+          cur = '?';
+        }
+      }
+      putchar(cur);
+      prev = cur;
+      }
     }
     fclose(fp);
     currfile++;
   }
-  free(str);
   return flag;
 }
