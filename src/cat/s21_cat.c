3acbae39beb6f19bb3ef38fee934f8a86944ff6c
Cass Curse <casscurs@vo-l4.msk.21-school.ru>
2022-11-09 15:18:35 +0300

    last

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -5,18 +5,18 @@ int main(int argc, char **argv) {
   opt options = {0};
   flag_turn(argc, argv, &options);
   int currfile = optind;
-  mainwork(argc, argv,&currfile,options);
+  mainwork(argc, argv, &currfile, options);
   return flag;
 }
-void mainwork(int argc, char **argv,int* currfile,opt options){
+void mainwork(int argc, char **argv, int *currfile, opt options) {
   int Nullflag = 0;
   FILE *fp = NULL;
   int cur;
   int prev = '\n';
-    int i = 1;
+  int i = 1;
   int empt = 0;
   while (*currfile <= argc) {
-        if (*currfile != argc) {
+    if (*currfile != argc) {
       fp = fopen(argv[*currfile], "r");
       if (fp == NULL) {
         printf("\ncat: %s: No such file\n", argv[*currfile]);
@@ -28,13 +28,13 @@ void mainwork(int argc, char **argv,int* currfile,opt options){
       }
     }
     while (!Nullflag && ((cur = fgetc(fp)) != EOF)) {
-      ifflag(&cur, &prev, &i,&empt, options);
+      ifflag(&cur, &prev, &i, &empt, options);
     }
     fclose(fp);
     (*currfile)++;
   }
 }
-void ifflag(int *cur, int *prev, int *i,int* empt, opt options) {
+void ifflag(int *cur, int *prev, int *i, int *empt, opt options) {
   int Deleteflag = 0;
   if (options.sflag && (*cur == '\n') && (*prev == '\n')) {
     (*empt)++;
@@ -80,34 +80,34 @@ void flag_turn(int argc, char **argv, opt *options) {
   int opindex = 0;
   while (-1 != (opchar = getopt_long(argc, argv, "+sbentv", opts, &opindex))) {
     switch (opchar) {
-    case 'b':
-      options->bflag = 1;
-      break;
-    case 'e':
-      options->eflag = 1;
-      options->vflag = 1;
-      break;
-    case 'n':
-      options->nflag = 1;
-      break;
-    case 's':
-      options->sflag = 1;
-      break;
-    case 't':
-      options->tflag = 1;
-      options->vflag = 1;
-      break;
-    case 'E':
-      options->eflag = 1;
-      break;
-    case 'T':
-      options->tflag = 1;
-      break;
-    case 'v':
-      options->vflag = 1;
-      break;
-    default:
-      printf("usage: cat [-benstuv] [file ...]\n");
+      case 'b':
+        options->bflag = 1;
+        break;
+      case 'e':
+        options->eflag = 1;
+        options->vflag = 1;
+        break;
+      case 'n':
+        options->nflag = 1;
+        break;
+      case 's':
+        options->sflag = 1;
+        break;
+      case 't':
+        options->tflag = 1;
+        options->vflag = 1;
+        break;
+      case 'E':
+        options->eflag = 1;
+        break;
+      case 'T':
+        options->tflag = 1;
+        break;
+      case 'v':
+        options->vflag = 1;
+        break;
+      default:
+        printf("usage: cat [-benstuv] [file ...]\n");
     }
   }
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -13,15 +13,15 @@ typedef struct options {
 } opt;
 
 struct option opts[] = {
-      {"squeeze-blank", no_argument, 0, 's'},
-      {"number-nonblank", no_argument, 0, 'b'},
-      {"E", no_argument, 0, 'e'},
-      {"number", no_argument, 0, 'n'},
-      {"T", no_argument, 0, 't'},
-      {"v", no_argument, 0, 'v'},
-      {0, 0, 0, 0},
-  };
+    {"squeeze-blank", no_argument, 0, 's'},
+    {"number-nonblank", no_argument, 0, 'b'},
+    {"E", no_argument, 0, 'e'},
+    {"number", no_argument, 0, 'n'},
+    {"T", no_argument, 0, 't'},
+    {"v", no_argument, 0, 'v'},
+    {0, 0, 0, 0},
+};
 
-void ifflag(int *cur, int *prev, int *i,int* empt, opt options);
+void ifflag(int *cur, int *prev, int *i, int *empt, opt options);
 void flag_turn(int argc, char **argv, opt *options);
-void mainwork(int argc, char **argv,int* currfile,opt options);
+void mainwork(int argc, char **argv, int *currfile, opt options);
